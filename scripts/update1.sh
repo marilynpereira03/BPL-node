004535fc11ea6d322817c17186e931ef62215b42
avinash <avinash.vazratkar@harbingergroup.com>
2018-01-04 15:55:30 +0530

    Changed command to start BPL-node

--- scripts/update1.sh
+++ scripts/update1.sh
@@ -4,8 +4,9 @@ mv config.wbx.json c.json
 mv genesisBlock.wbx.json g.json
 git checkout config.wbx.json
 git checkout genesisBlock.wbx.json
-git pull origin wbx >> aa.txt
-mv  c.json config.wbx.json
-mv  g.json genesisBlock.wbx.json
-kill `lsof -t -i:9032`
-node app.js -c config.wbx.json -g genesisBlock.wbx.json >> a.txt &
+git pull origin wbx
+mv c.json config.wbx.json
+mv g.json genesisBlock.wbx.json
+forever stop 9032
+# kill `lsof -t -i:9032`
+forever start app.js -c config.wbx.json -g genesisBlock.wbx.json
