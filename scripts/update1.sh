14add2684fad3e2ffba4550b6b38d2f966b9188c
avinash <avinash.vazratkar@harbingergroup.com>
2018-01-04 17:50:21 +0530

    Script changed

--- scripts/update1.sh
+++ scripts/update1.sh
@@ -2,11 +2,9 @@
 #
 mv config.wbx.json c.json
 mv genesisBlock.wbx.json g.json
-git checkout config.wbx.json
-git checkout genesisBlock.wbx.json
-git pull origin wbx
+git pull origin wbx-avi
 mv c.json config.wbx.json
 mv g.json genesisBlock.wbx.json
-forever stop 9032
+forever stop `lsof -t -i:9032`
 # kill `lsof -t -i:9032`
 forever start app.js -c config.wbx.json -g genesisBlock.wbx.json
